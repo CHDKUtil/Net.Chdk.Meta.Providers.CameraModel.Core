742b393280c79a44e66e7c7f43cd65efd18c64b9
Dmitry Shechtman <damnian@gmail.com>
2017-07-05 00:23:35 +0300

    Add ICameraListProvider

---
+++ src/Net.Chdk.Meta.Providers.CameraModel.Core/ICameraListProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Net.Chdk.Meta.Providers.CameraModel
+{
+    public interface ICameraListProvider
+    {
+        IDictionary<string, IDictionary<string, string>> GetCameraList(Stream stream);
+    }
+}
