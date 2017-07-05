33a790825a26058e4dd2c778828513f286334f9a
Dmitry Shechtman <damnian@gmail.com>
2017-07-05 06:04:27 +0300

    Add ICameraTreeProvider

---
+++ src/Net.Chdk.Meta.Providers.CameraModel.Core/ICameraTreeProvider.cs
@@ -0,0 +1,11 @@
+﻿using Net.Chdk.Meta.Model.CameraModel;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Net.Chdk.Meta.Providers.CameraModel
+{
+    public interface ICameraTreeProvider
+    {
+        IDictionary<string, IDictionary<string, TreeRevisionData>> GetCameraTree(Stream stream);
+    }
+}
